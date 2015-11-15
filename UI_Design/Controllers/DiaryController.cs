ffaefff08f23dacadbeb88ad8e9656e5a5e1937f
Azure <ruttikar_maya@hotmail.com>
2015-11-16 04:49:41 +0700

    Initial

---
+++ UI_Design/Controllers/DiaryController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI_Design.Controllers
+{
+    public class DiaryController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your My Cats page.";
+
+            return View();
+        }
+
+        public ActionResult DashBoard()
+        {
+            ViewBag.Message = "Dash board.";
+
+            return View();
+        }
+
+        public ActionResult RitaDiary()
+        {
+            ViewBag.Message = "Rita's Diary.";
+
+            return View();
+        }
+
+        public ActionResult RitaDiarySidebar()
+        {
+            ViewBag.Message = "Rita's Diary.";
+
+            return View();
+        }
+
+        public ActionResult AddCat()
+        {
+            ViewBag.Message = "Add new cat.";
+
+            return View();
+        }
+    }
+}
